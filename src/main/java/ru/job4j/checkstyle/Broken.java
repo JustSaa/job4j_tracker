742daac9a5c268dc37512b87ed841c3e7aea3e99
Salavat <Justdrumm@yandex.ru>
2024-08-26 21:03:28 +0300

    Правила Checkstyle

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    private String name;
+    private String surname;
+    private int size = 10;
+
+    public Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c) {
+    }
+}
