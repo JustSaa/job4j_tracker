a2d2f513a6119341ad70a78c74d151fd6f1b86aa
Salavat <Justdrumm@yandex.ru>
2024-09-03 20:13:39 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active = " + active);
+        System.out.println("Status = " + status);
+        System.out.println("Message = " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 2, "Ошибка пол номером 2");
+        Error error3 = new Error(false, 3, "Ошибка пол номером 3");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
