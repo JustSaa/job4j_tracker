5ca831fa6fc652a7eabbf2852e3a43a996109d82
Salavat <Justdrumm@yandex.ru>
2024-12-19 19:55:26 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,25 @@ public class Tracker {
             }
         }
         return result;
+    }
+
+    public Item[] findAll() {
+        Item[] allItems = new Item[size];
+        for (int i = 0; i < size; i++) {
+            allItems[i] = items[i];
+        }
+        return allItems;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] allItems = new Item[size];
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                allItems[i] = items[i];
+                counter++;
+            }
+        }
+        return Arrays.copyOf(allItems, counter);
     }
 }
