5869749380d7499993dad90287bea3be05f2af7f
Salavat <Justdrumm@yandex.ru>
2025-01-12 19:02:20 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.MockInput;
+import ru.job4j.tracker.input.ValidateInput;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenValidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"10"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(10);
+    }
+
+    @Test
+    void whenMultipleValidInput() {
+        Output output = new StubOutput();
+        String[] inputs = new String[]{"10", "9", "3", "4"};
+        Input in = new MockInput(inputs);
+        ValidateInput input = new ValidateInput(output, in);
+        for (String s : inputs) {
+            int selected = input.askInt("Enter menu:");
+            assertThat(selected).isEqualTo(Integer.parseInt(s));
+        }
+    }
+
+    @Test
+    void whenMinusValidInput() {
+        Output output = new StubOutput();
+        Input in = new MockInput(
+                new String[]{"-10"}
+        );
+        ValidateInput input = new ValidateInput(output, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-10);
+    }
+}
