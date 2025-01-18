d72f795b25b959d11c576b9e2cd14b8f614548d0
Salavat <Justdrumm@yandex.ru>
2025-01-18 21:15:32 +0300

    refactor: added double key

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -8,6 +8,7 @@ public class UsageMap {
         Map<String, String> map = new HashMap<>();
         map.put("ivan@gmail.com", "Ivanov");
         map.put("petr@gmail.com", "Petrovich");
+        map.put("petr@gmail.com", "Hoow");
         for (String email : map.keySet()) {
             System.out.println(email + " - " + map.get(email));
         }
