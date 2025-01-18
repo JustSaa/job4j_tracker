6cc890a03c38313f76b833388af07bc5406b619d
Salavat <Justdrumm@yandex.ru>
2025-01-18 21:00:38 +0300

    refactor: deleted streams and added loo on FullSearch class

--- src/main/java/ru/job4j/collection/FullSearch.java
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -1,14 +1,17 @@
 package ru.job4j.collection;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class FullSearch {
 
     public Set<String> extractNumber(List<Task> tasks) {
-        return tasks.stream()
-                .map(Task::getNumber)
-                .collect(Collectors.toSet());
+        Set<String> search = new HashSet<>();
+
+        for (Task task : tasks) {
+            search.add(task.getNumber());
+        }
+        return search;
     }
 }
