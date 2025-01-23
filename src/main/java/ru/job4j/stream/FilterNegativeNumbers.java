6fdc7fbada2831961861475911f7c723ac0a0c46
Salavat <Justdrumm@yandex.ru>
2025-01-23 19:57:21 +0300

    refactor: changes on method at FilterNeg class

--- src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
+++ src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
@@ -8,7 +8,7 @@ public class FilterNegativeNumbers {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, -22, 2, -1, 5, 100);
-        List<Integer> positive = numbers.stream().filter(number -> number > 0).toList();
+        List<Integer> positive = numbers.stream().filter(number -> number > 0).collect(Collectors.toList());
         positive.forEach(System.out::println);
     }
 }
