51a07abb3cd91c1baef3a6d8008d8aaf5d861e4e
Salavat <Justdrumm@yandex.ru>
2025-01-24 20:06:07 +0300

    refactor: changed at PhoneDic class

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -17,11 +17,14 @@ public class PhoneDictionary {
      * @return Список пользователей, которые прошли проверку.
      */
     public ArrayList<Person> find(String key) {
-        Predicate<Person> combine = person ->
-                person.getName().contains(key)
-                        || person.getSurname().contains(key)
-                        || person.getPhone().contains(key)
-                        || person.getAddress().contains(key);
+        Predicate<Person> namePredicate = person -> person.getName().contains(key);
+        Predicate<Person> phonePredicate = person -> person.getPhone().contains(key);
+        Predicate<Person> addressPredicate = person -> person.getAddress().contains(key);
+        Predicate<Person> surnamePredicate = person -> person.getSurname().contains(key);
+        Predicate<Person> combine = namePredicate
+                .or(phonePredicate)
+                .or(addressPredicate)
+                .or(surnamePredicate);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
             if (combine.test(person)) {
