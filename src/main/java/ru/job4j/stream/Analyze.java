092df681e283d0a2610661bdb1a21b81fbebd039
Salavat <Justdrumm@yandex.ru>
2025-01-27 20:42:39 +0300

    3. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,48 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.LinkedHashMap;
+
+import static java.util.stream.Collectors.*;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.subjects().stream())
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(0D);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.map(pupil -> new Tuple(pupil.name(), pupil.subjects().stream()
+                        .mapToInt(Subject::score).average().orElse(0D)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.subjects().stream())
+                .collect(groupingBy(Subject::name, LinkedHashMap::new, averagingDouble(Subject::score)))
+                .entrySet().stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(pupil -> new Tuple(pupil.name(), pupil.subjects().stream()
+                        .mapToInt(Subject::score).sum()))
+                .max(Comparator.comparing(Tuple::score))
+                .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.subjects().stream())
+                .collect(groupingBy(Subject::name, LinkedHashMap::new, summingDouble(Subject::score)))
+                .entrySet().stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .max(Comparator.comparing(Tuple::score))
+                .orElse(null);
+    }
+}
