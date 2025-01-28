7f648b3e8dff14fa2d1d68e4e82d9c510dfcc2f8
Salavat <Justdrumm@yandex.ru>
2025-01-28 21:01:41 +0300

    Combined all changes into one commit

---
+++ src/main/java/ru/job4j/test/TestClass.java
@@ -0,0 +1,11 @@
+package ru.job4j.test;
+
+public class TestClass {
+    public boolean testMethod(String test) {
+        return false;
+    }
+
+    public String testMethodForString() {
+        return "Test";
+    }
+}
